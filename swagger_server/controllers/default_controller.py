282723b5847efa219dbb513c328ece4d32a0e826
Borja Velasco Santamaría <borja.velasco.st@gmail.com>
2024-02-09 19:04:29 +0100

    controller modification wrong

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,3 +1,4 @@
+from swagger_server.service.student_service import *
 import connexion
 import six
 
@@ -5,19 +6,20 @@ from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
 
-def add_student(body=None):  # noqa: E501
+def add_student(body=None): # noqa: E501
     """Add a new student
 
     Adds an item to the system # noqa: E501
 
     :param body: Student item to add
     :type body: dict | bytes
-
+    
     :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500,'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +32,9 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return delete(student_id)
+    return 500,'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +47,6 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return get_by_id(student_id)
+    return 500,'error'
